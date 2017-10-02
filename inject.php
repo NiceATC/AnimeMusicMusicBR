ccebc7400aaac9d2cb079066ad849daf35d3c7c6
NiceATC <NiceATC@users.noreply.github.com>
2017-10-02 00:36:17 -0300

    Add files via upload

---
+++ inject.php
@@ -0,0 +1,55 @@
+
+{
+    "room": "World Of Pop | Brasil",
+    "author": "-DATABASE-",
+    "css": "",
+    "icon": "https://i.imgur.com/ygXrAY0.png",
+    
+    "rules": {
+        "allowAutorespond": true,
+        "allowAutowoot": true,
+        "allowAutojoin": false,
+        "allowAutograb": true,
+        "allowSmartVote": true,
+        "allowEmotes": true,
+        "allowShowingMehs": true,
+        "forceSmartVote": false
+    },
+    "emotes": {
+        "wop": "https://i.imgur.com/ygXrAY0.png"
+    }, 
+     "autocomplete": {
+         "!commands": 0,
+         "!dc": 0,
+         "!rules": 0,
+         "!ping": 0,
+         "!blacklist": 0,
+         "!website": 0,
+         "!link": 0,
+         "!8ball": 0,
+         "!gif": 0,
+         "!cookie": 0,
+         "!skip": 2,
+         "!lockskip": 2,
+         "!add": 2,
+         "!remove": 2,
+         "!move": 2,
+         "!lock": 2,
+         "!unlock": 2,
+         "!kick": 2,
+         "!ban": 2,
+         "!unban": 2,
+         "!mute": 2,
+         "!unmute": 2,
+         "!say": 3,
+         "!motd": 3,
+         "!togglemotd": 3,
+         "!kill": 3,
+         "!reload": 3,
+         "!uptime": 3,
+         "!status": 3,
+         "!swap": 3,
+
+
+     }
+}
